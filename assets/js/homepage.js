01ff9c9df7041b3642f3b6e530a29b4903a0d8f7
Hok S. Uy <hoksuy@gmail.com>
2021-07-18 10:21:34 -0700

    added api fetching and display

---
+++ assets/js/homepage.js
@@ -0,0 +1,72 @@
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    }).catch(function(error) {
+        alert("Unable to connect to GitHub");
+    });
+}
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var reposContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+    if(repos.length === 0) {
+        reposContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
+    reposContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    for(var i = 0; i < repos.length; i++) {
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        var repoEl = document.createElement("div");
+        
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        repoEl.appendChild(titleEl);
+
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        if(repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issues(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>"
+        }
+
+        repoEl.appendChild(statusEl);
+        reposContainerEl.appendChild(repoEl);
+    }
+    
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
